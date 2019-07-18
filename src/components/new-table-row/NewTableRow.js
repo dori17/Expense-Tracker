197084f75ddc20b225dd7f81dd7fe237db2e4be6
darya.bondarava <darya.bondarava@itechart-group.com>
2019-07-18 13:33:13 +0300

    currency field made select (with options)

--- src/components/new-table-row/NewTableRow.js
+++ src/components/new-table-row/NewTableRow.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './NewTableRow.css'
 import Button from "../button/Button";
+import {Currency} from "../../App";
 
 class NewTableRow extends React.PureComponent {
     constructor(props) {
@@ -14,15 +15,24 @@ class NewTableRow extends React.PureComponent {
 
     render() {
         const {onAdd, onCancel} = this.props;
+        let selectOptions = [];
+        for(const currency of Object.values(Currency)) {
+            selectOptions.push(<option key={currency} value={currency}>{currency}</option>)
+        }
+
         return (
             <div className="new-table-row">
                 <input className="input-new-line" type="text" placeholder="Name" name="person" onChange={this.handleValueChange}/>
                 <input className="input-new-line" type="number" placeholder="Amount" name="amount" onChange={this.handleValueChange}/>
-                <input className="input-new-line" type="text" placeholder="Currency" name="currency" onChange={this.handleValueChange}/>
+                <select name="currency" onChange={this.handleValueChange}>
+                    {selectOptions}
+                </select>
                 <input className="input-new-line" type="date" placeholder="Date" name="date" onChange={this.handleValueChange}/>
                 <input className="input-new-line" type="text" placeholder="Description" name="description" onChange={this.handleValueChange}/>
+                <div className="flex-row">
                 <Button className="button-add" onClick={() => onAdd(this.state)}>Add</Button>
                 <Button className="button-cancel" onClick={onCancel}>Cancel</Button>
+                </div>
             </div>
         )
     }
